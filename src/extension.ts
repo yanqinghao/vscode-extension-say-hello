7080d414cd8fa556cbb07fee910527c0ac56e899
YanQinghao <woshiyanqinghao@gmail.com>
2020-08-07 17:38:25 +0800

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as OSS from 'ali-oss';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,19 +10,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "say-hello" is now active!');
+	const client = new OSS({
+		region: 'oss-cn-beijing',
+		accessKeyId: '',
+		accessKeySecret: '',
+		bucket: 'suanpan',
+		// stsToken: '<security-token>'
+	});
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('say-hello.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('say-hello.helloWorld', (uri: vscode.Uri) => {
 		// The code you place here will be executed every time your command is executed
-
+		// client.put('common/model/obj', uri.fsPath);
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from say-hello!');
+		// 'Hello World from say-hello!' + uri.fsPath
+		let envGet = vscode.window.activeTextEditor?.document.uri.path
+		if (typeof (envGet) != "undefined") { 
+			vscode.window.showInformationMessage(envGet);
+		} else {
+			vscode.window.showInformationMessage("undefined");
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
